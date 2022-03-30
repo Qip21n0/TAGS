543d6c19c045df83c6a96a2e1d414afce9e43cb9
Kenn <h.ken.40nuj7991@icloud.com>
2022-03-31 06:20:02 +0900

    completed funcs for compile & test

--- tags/func.py
+++ tags/func.py
@@ -1,3 +1,13 @@
+from util import *
+from tqdm import tqdm
+from zipfile import ZipFile
+import subprocess
+import difflib
+import glob
+import sys
+
+
+
 def download(url):
 	"""
 	This is the test.
@@ -19,17 +29,90 @@ def download(url):
 	"""
 	print("OK")
 
+
 def unzip(path):
 	"""
 	"""
-	pass
+	with ZipFile(path, 'r') as zip:
+		zip.extractall()
 
-def compile(path):
-	"""
-	"""
-	pass
 
-def test(path):
+def compile(ext):
 	"""
 	"""
-	pass
+	student_id = get_config()
+	if ext == 'cpp' or 'c':
+		for id in tqdm(student_id):
+			command = 'gcc -g -Wall ' + id + '.' + ext + ' -o ' + id
+			subprocess.run(command, shell=True)
+		print("COMPILE COMPLETED!!")
+		
+	else:
+		print("ERROR: Invalid file extension.")
+		sys.exit(0)
+	
+
+def test(modified):
+	"""
+	"""
+	test_path = './test.txt'
+	answer_path = './answer.txt'
+
+	if test_path not in glob.glob('./*.txt') or \
+		answer_path not in glob.glob('./*.txt') or\
+		modified:
+		print("Write some tests & answers.")
+		print("(Example) \ntest <= 1 2 \nanswer <= 4\n")
+		count = 0
+		tests = []
+		answers = []
+		while 1:
+			test = input(f'test[{count}] <= ')
+			answer = input(f'answer[{count}] <= ')
+			if test == '' or answer == '':
+				break
+			else:
+				tests.append(test)
+				answers.append(answer)
+				count += 1
+		
+		with open(test_path, 'w') as f:
+			f.write('\n'.join(tests))
+		with open(answer_path, 'w') as f:
+			f.write('\n'.join(answers))
+
+		print()
+	
+	print('\033[33m'+'TEST START'+'\033[0m\n')
+	student_id = get_config()
+	tests = []
+	answers = []
+	with open(test_path, 'r') as f:
+		lines = f.read()
+		for line in lines.split("\n"):
+			tests.append(line)
+
+	with open(answer_path, 'r') as f:
+		lines = f.read()
+		for line in lines.split("\n"):
+			answers.append(line)
+
+	for id in student_id:
+		print("student: " + '\033[31m'+f'{id}'+'\033[0m\n')
+		score = len(answers)
+		for i, t in enumerate(tests):
+			if t == '':
+				break
+			print('\033[32m'+f'TEST[{i}]'+'\033[0m'+'\t\t'+'\033[34m'+f'ANSWER[{i}]'+'\033[0m')
+			print('\t\t'+answers[i], end='')
+			command = 'echo ' + t + ' | ' + './' + id + ' | tee diff.txt'
+			subprocess.run(command, shell=True)
+			
+			with open('./diff.txt', 'r') as f:
+				out = f.read()
+				if answers[i] != out:
+					score -= 1
+
+			print()
+		print(f'SCORE: {score}/{len(answers)}')
+		print('=' * 24)
