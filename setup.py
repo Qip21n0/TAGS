c155c56067c5c015e5f65fca48fed1928b84a97a
Kenn <h.ken.40nuj7991@icloud.com>
2022-04-02 04:48:27 +0900

    released TAGS

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="tags",
+    version="0.1.0",
+    author="Ken Hisanaga",
+    author_email="Xenn.1i82@gmail.com",
+    description="Commands used by the TA for grading",
+    url="https://github.com/Qip21n0/TAGS",
+	install_requires=[
+		'click',
+		'tqdm',
+		'pandas',
+	], 
+    packages=setuptools.find_packages(),
+    entry_points = {
+        'console_scripts': ['tags = tags.main:main']
+    },
+    python_requires='>=3.8',
+)
