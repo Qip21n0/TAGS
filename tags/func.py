873c49719a9c123604e9f3229d7f6d3698d08cde
Kenn <h.ken.40nuj7991@icloud.com>
2022-04-03 08:47:18 +0900

    completed func for download & add docstrings

--- tags/func.py
+++ tags/func.py
@@ -1,31 +1,38 @@
-from tags.util import *
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 from tqdm import tqdm
+from tags.util import *
+import chromedriver_binary
+import pyautogui
 import subprocess
 import shutil
 import glob
-import sys
+import time
 import os
 
 
 
-def download(r):
+def download(report):
 	"""
-	This is the test.
+	Download zip files.
+
+	Visit the page corresponding to the given assignment
+	and automatically download all zip files with GUI. 
 
 	Parameters
 	--------
-	url : str
-	    url to get
+	r : str
+	    ex) T5, E12
 
 	Returns
 	--------
-	str or None
-	    Return value
+	None
 
 	Raises
 	--------
 	ValueError
 	    if url is invalid.
+	
 	"""
 	data = get_config()
 	tmp = 'tmp'
@@ -33,13 +40,53 @@ def download(r):
 		os.mkdir(tmp)
 
 	download_path = data['dir'] + '/' + tmp
-	url = data['url'] + '&act_report=1&c=' + data['class'] + '&r=' + r
+	url = data['url'] + '&act_report=1&c=' + data['class'] + '&r=' + report
 
-	print("OK")
+	options = webdriver.ChromeOptions()
+	options.add_experimental_option('prefs', {'download.default_directory': download_path})
+
+	browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+	browser.get(url)
+	time.sleep(1)
+
+	pyautogui.typewrite(data['id'])
+	pyautogui.press('tab')
+	pyautogui.typewrite(data['psswd'])
+	pyautogui.press('enter')
+	time.sleep(2)
+	browser.get(url)
+	time.sleep(1)
+
+	elements = browser.find_elements_by_xpath("//a[@href]")
+	zipfile_url = set()
+	for element in elements:
+		href = element.get_attribute('href')
+		if '&act_download_multi=1'in href:
+			zipfile_url.add(href)
+		else:
+			continue
+	
+	for i in zipfile_url:
+		browser.get(i)
+		time.sleep(1)
+	
+	browser.quit()
 
 
 def unzip():
 	"""
+	Unzip zip files.
+
+	Unzip and delete all zip files in `tmp/` directory
+
+	Rapameters
+	--------
+	None
+
+	Returns
+	--------
+	None
+
 	"""
 	data = get_config()
 	dir = data['dir'] + '/tmp'
@@ -54,23 +101,56 @@ def unzip():
 
 def compile(ext):
 	"""
+	Compile c or cpp files.
+
+	Compile only the student assignment files whose
+	contents have been changed. 
+
+	Parameters
+	--------
+	ext : str
+	    Extension type (ex. c cpp)
+
+	Returns
+	--------
+	None
+
+	Raise
+	--------
+	ValueError
+	    if `ext` is not 'c' or 'cpp', outputs "Invalid
+		file extension".
+
 	"""
 	student_id = logging(ext)
 	if ext == 'cpp' or 'c':
 		for id in tqdm(student_id):
 			id = str(id)
-			#command = ['gcc', '-g', '-Wall', id+'.'+ext, '-o', id]
-			command = 'gcc -g -Wall ' + id + '.' + ext + ' -o ' + id
+			file = id + '.' + ext
+			compiler = 'gcc' if ext == 'c' else 'g++'
+
+			command = compiler + ' -g -Wall ' + file + ' -o ' + id
 			subprocess.run(command, shell=True)
 		print("COMPILE COMPLETED!!")
 		
 	else:
-		print("ERROR: Invalid file extension.")
-		sys.exit(0)
+		raise ValueError("ERROR: Invalid file extension.")
 	
 
 def test(modified):
 	"""
+	function for test.
+
+	Parameters
+	--------
+	modified : boolean
+	    modify the test items in test.txt when
+		`modified` is True.
+
+	Returns
+	--------
+	None
+
 	"""
 	test_path = './test.txt'
 	answer_path = './answer.txt'
@@ -100,7 +180,7 @@ def test(modified):
 
 		print()
 	
-	print('\033[33m'+'TEST START'+'\033[0m\n')
+	print('\033[33m'+'TEST START'+'\033[0m')
 	data = get_config()
 	student_id = data['student_id']
 	tests = []
@@ -116,25 +196,32 @@ def test(modified):
 			answers.append(line)
 
 	for id in student_id:
+		print('=' * 32)
 		print("student: " + '\033[31m'+f'{id}'+'\033[0m\n')
-		score = len(answers)
+		score = 0
+		id = str(id)
+		if './'+id not in glob.glob('./*'):
+			print(f'ERROR: No executable file of student {id}')
+			continue
+
 		for i, t in enumerate(tests):
 			if t == '':
 				break
 			print('\033[32m'+f'TEST[{i}]'+'\033[0m'+'\t\t'+'\033[34m'+f'ANSWER[{i}]'+'\033[0m')
-			print('\t\t'+answers[i], end='')
-			id = str(id)
-			#command = ['echo', t, '|', './'+id, '|', 'tee', 'diff.txt']
-			command = 'echo ' + t + ' | ' + './' + id + ' | tee diff.txt'
-			subprocess.run(command, shell=True)
 			
-			with open('./diff.txt', 'r') as f:
-				out = f.read()
-				if answers[i] != out:
-					score -= 1
+			p1 = subprocess.Popen(['echo', t], stdout=subprocess.PIPE)
+			p2 = subprocess.Popen(['./'+id], stdin=p1.stdout, stdout=subprocess.PIPE)
+			p1.stdout.close()
+			output = p2.communicate()[0].decode()
+
+			print(output+'\t\t'+answers[i])
+			answer = answers[i].split()
+			flag = True
+			for a in answer:
+				if a not in output:
+					flag = False
+			if flag:
+				score += 1
 
 			print()
 		print(f'SCORE: {score}/{len(answers)}')
-		print('=' * 24)
-		
-	os.remove('./diff.txt')
