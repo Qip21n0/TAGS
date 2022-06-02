11f7b06d1681c24d59c08ae25c627226f8d38bd1
Kenn <h.ken.40nuj7991@icloud.com>
2022-06-02 11:43:07 +0900

    modified typo

--- tags/repl.py
+++ tags/repl.py
@@ -108,14 +108,14 @@ class TagsCmd(Cmd):
 
 	def do_exe(self, line):
 		data = get_config()
-		student_id = data['id']
-		try:
+		student_id = data['student_id']
+		"""try:
 			exe_file = int(line)
 		except:
 			print("ERROR: invalid input.")
 			return None
-			
-		if exe_file not in student_id:
+"""
+		if line not in student_id:
 			print("ERROR: No executable file exists for the student number entered.")
 		else:
 			subprocess.run('.'+SLASH+line, shell=True)
