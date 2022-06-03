c71235e95db00e1d0461a3338acae084e98ad7f7
Kenn <h.ken.40nuj7991@icloud.com>
2022-06-03 19:53:34 +0900

    updated for #9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name="tags",
-    version="0.1.5dev",
+    version="0.1.6",
     author="Ken Hisanaga",
     author_email="Xenn.1i82@gmail.com",
     description="Commands used by the TA for grading",
