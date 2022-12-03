fd8f922725b0e9ff1c000fe994ff80d99b2f891f
Kenn <h.ken.40nuj7991@icloud.com>
2022-12-03 23:53:53 +0900

    comment out path for history & last directory

--- tags/function/base.py
+++ tags/function/base.py
@@ -5,37 +5,22 @@ import json
 
 class BasicTAGS:
 	def __init__(self):
-		self.home_path = os.path.expanduser('~')
-		self.tags_path = self._get_tags_path()
-		self.config_path = os.path.join(self.tags_path, 'config.json')
-		self.history_path = os.path.join(self.tags_path, 'history.txt')
-		self.last_dir_path = os.path.join(self.tags_path, 'last_dir.txt')
+		tags_path = self._get_tags_path()
+		config_path = os.path.join(tags_path, 'config.json')
+		with open(config_path, 'r') as f:
+			self.config_data = json.load(f)
+		#self.history_path = os.path.join(self.tags_path, 'history.txt')
+		#self.last_dir_path = os.path.join(self.tags_path, 'last_dir.txt')
 
 	
 	def _get_tags_path(self):
+		home_path = os.path.expanduser('~')
 		cwd = os.getcwd()
 		paths = []
-		for path, _, _ in os.walk(self.home_path):
+		for path, _, _ in os.walk(home_path):
 			if path.endswith('.tags'):
 				paths.append(path[:-6])
 	
 		for path in paths:
 			if path in cwd:
 				return os.path.join(path, '.tags')
-
-
-	def load(self, filename):
-		if filename == 'config':
-			with open(self.config_path, 'r') as f:
-				self.config_data = json.load(f)
-
-		elif filename == 'history':
-			with open(self.history_path, 'r') as f:
-				self.history = f.readlines()
-
-		elif filename == 'last_dir':
-			with open(self.last_dir_path, 'r') as f:
-				self.last_dir = f.readlines()[-1]
-
-		else:
-			raise NameError("Not valid file name.")
